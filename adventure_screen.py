e64c1b5d3e4d421d37c4d5822bda40eb56763ccc
armnejdin <61328872+armnejdin@users.noreply.github.com>
2020-03-07 12:06:56 +0100

    Add files via upload

--- adventure_screen.py
+++ adventure_screen.py
@@ -3,8 +3,12 @@ import pygame
 #initiazile the pygame
 pygame.init()
 
-#creating the screen
-screen = pygame.display.set_mode((1200, 1000))
+#creating the screens
+player_screen = pygame.display.set_mode((1200, 950))
+#mountains
+background_screen = pygame.display.set_mode((1200,950))
+mountain = pygame.image.load("mountain.png")
+
 
 #title and icon
 pygame.display.set_caption("Adventure")
@@ -13,23 +17,60 @@ pygame.display.set_icon(icon)
 
 #Player
 playerimg = pygame.image.load("character_standingbig.png")
-playerx =
-playery = 0
+playerx = 400
+playery = 400
+playerx_change = 0
+playery_change = 0
 
-def player():
-    screen.blit(playerimg, [playerx, playery])
+#player function
+def player(x,y):
+    player_screen.blit(playerimg, [x, y])
 
 #Game loop
 running = True
 while running:
 
     # rgb colour
-    screen.fill((0, 0, 0))
+    player_screen.fill((0, 0, 0))
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+     #if key pressed check if left or right
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerx_change -= 5
+            if event.key == pygame.K_RIGHT:
+                playerx_change += 5
+            if event.key == pygame.K_UP:
+                playery_change -= 5
+            if event.key == pygame.K_DOWN:
+                playery_change += 5
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT or event.key == pygame.K_DOWN or event.key == pygame.K_UP :
+                playerx_change = 0
+                playery_change = 0
+
+
+
+    #player movement
+    playerx += playerx_change
+    playery += playery_change
+
+    if playerx <= 0:
+        playerx = 0
+    elif playerx >= 1000:
+        playerx = 1000
+    if playery <= 0:
+        playery = 0
+    elif playery >= 750:
+        playery = 750
+
+    # background
+    player_screen.blit(mountain, [300, 400])
+
     #call player function
-    player()
+    player(playerx, playery)
     pygame.display.update()
+
